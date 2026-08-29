73f62ee22eeddc8f0caacff1a43677f6b796806c
zahid5656 <zahid5656@gmail.com>
2026-08-29 16:39:25 +0600

    debug: twrp theme not found build error resolve testing

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fox_samurai.mk \
     $(LOCAL_DIR)/twrp_samurai.mk
 
 COMMON_LUNCH_CHOICES := \
-    fox_samurai-eng \
-    fox_samurai-userdebug \
     twrp_samurai-eng
